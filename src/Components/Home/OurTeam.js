8d9a86c2e4adb4b531c7a39f697ae6ccc034a2c2
mostafiz <rafi11zxy@gmail.com>
2022-11-12 04:28:49 +0600

    ourteam section update

--- src/Components/Home/OurTeam.js
+++ src/Components/Home/OurTeam.js
@@ -7,7 +7,7 @@ const OurTeam = () => {
         <div className="xl:container mx-auto px-6 md:px-12">
           <div className="mb-16 md:w-2/3 lg:w-1/2 flex justify-center">
             <h2 className="mb-4 text-2xl font-bold text-gray-800 dark:text-white md:text-4xl">
-              Our Doctor
+              Our Specialist Doctor
             </h2>
             {/* <p className="text-gray-600 dark:text-gray-300">
               Tailus prides itself not only on award-winning technology, but
@@ -19,7 +19,7 @@ const OurTeam = () => {
             <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
               <img
                 className="mx-auto h-[26rem] w-full grayscale object-cover object-top ransition duration-500 group-hover:scale-105 group-hover:grayscale-0"
-                src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                src="https://www.soothing.dental/wp-content/uploads/2022/07/dehghani-slide-min.jpg"
                 alt="men"
                 loading="lazy"
                 width={640}
@@ -28,46 +28,16 @@ const OurTeam = () => {
               <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-gray-800 dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
                 <div>
                   <h4 className="text-xl font-semibold dark:text-gray-700 text-white">
-                    Hentoni Doe
+                    Hossein Dehani
                   </h4>
-                  <span className="block text-sm text-gray-500">
-                    CEO-Founder
-                  </span>
+                  <span className="block text-sm text-gray-500">CEO</span>
                 </div>
-                <p className="mt-8 text-gray-300 dark:text-gray-600">
-                  Quae labore quia tempora dolor impedit. Possimus, sint ducimus
-                  ipsam?
-                </p>
-              </div>
-            </div>
-            <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
-              <img
-                className="mx-auto h-[26rem] w-full grayscale object-cover object-top ransition duration-500 group-hover:scale-105 group-hover:grayscale-0"
-                src="https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fHdvbWVuJTIwcG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-                alt="woman"
-                loading="lazy"
-                width={640}
-                height={805}
-              />
-              <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-gray-800 dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
-                <div>
-                  <h4 className="text-xl font-semibold dark:text-gray-700 text-white">
-                    Hentoni Doe
-                  </h4>
-                  <span className="block text-sm text-gray-500">
-                    CEO-Founder
-                  </span>
-                </div>
-                <p className="mt-8 text-gray-300 dark:text-gray-600">
-                  Quae labore quia tempora dolor impedit. Possimus, sint ducimus
-                  ipsam?
-                </p>
               </div>
             </div>
             <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
               <img
                 className="mx-auto h-[26rem] w-full grayscale object-cover object-top transition duration-500 group-hover:scale-105 group-hover:grayscale-0"
-                src="https://images.unsplash.com/photo-1628890923662-2cb23c2e0cfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                src="https://www.soothing.dental/wp-content/uploads/2022/07/saeidi-slide-min.jpg"
                 alt="woman"
                 loading="lazy"
                 width={640}
@@ -76,16 +46,30 @@ const OurTeam = () => {
               <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-gray-800 dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
                 <div>
                   <h4 className="text-xl font-semibold dark:text-gray-700 text-white">
-                    Hentoni Doe
+                    Sona Saeidi
+                  </h4>
+                  <span className="block text-sm text-gray-500">Dentist</span>
+                </div>
+              </div>
+            </div>
+            <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
+              <img
+                className="mx-auto h-[26rem] w-full grayscale object-cover object-top transition duration-500 group-hover:scale-105 group-hover:grayscale-0"
+                src="https://www.soothing.dental/wp-content/uploads/2022/07/misyuk-slide-min.jpg"
+                alt="woman"
+                loading="lazy"
+                width={640}
+                height={805}
+              />
+              <div className="absolute bottom-0 inset-x-0 h-max mt-auto px-8 py-6 bg-gray-800 dark:bg-white translate-y-24 transition duration-300 ease-in-out group-hover:translate-y-0">
+                <div>
+                  <h4 className="text-xl font-semibold dark:text-gray-700 text-white">
+                    ladan Sahabi
                   </h4>
                   <span className="block text-sm text-gray-500">
-                    CEO-Founder
+                    Associate Dentist
                   </span>
                 </div>
-                <p className="mt-8 text-gray-300 dark:text-gray-600">
-                  Quae labore quia tempora dolor impedit. Possimus, sint ducimus
-                  ipsam?
-                </p>
               </div>
             </div>
             <div className="group relative rounded-3xl  space-y-6 overflow-hidden">
@@ -103,13 +87,9 @@ const OurTeam = () => {
                     Hentoni Doe
                   </h4>
                   <span className="block text-sm text-gray-500">
-                    CEO-Founder
+                    Dentist
                   </span>
                 </div>
-                <p className="mt-8 text-gray-300 dark:text-gray-600">
-                  Quae labore quia tempora dolor impedit. Possimus, sint ducimus
-                  ipsam?
-                </p>
               </div>
             </div>
           </div>
